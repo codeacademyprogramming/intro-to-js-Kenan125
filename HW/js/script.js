10c8c47de6c2ffdd72335f703f2eb72cf8e076bd
Kanan Ismayilzada <7609260+Kenan125@users.noreply.github.com>
2021-07-05 15:31:31 +0400

    HW

---
+++ HW/js/script.js
@@ -0,0 +1,15 @@
+
+
+  function myFunction() {
+    var person = prompt("Please enter your name");
+    if (person != null) {
+      var age_verification = prompt("Please enter your age");
+      if(age_verification <18){
+          alert("you cannot enter");
+      } 
+      else{
+          alert("you can enter");
+      }
+    }
+  }
+
